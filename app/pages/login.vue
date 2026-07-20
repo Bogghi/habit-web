d52d1c814edb024ce370cdf8b9295f5413e3afad
Bogghi <m.borghi@tecnocasa.com>
2026-07-20 16:51:02 +0200

    commit first interaction

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -7,22 +7,39 @@
         <form class="space-y-6 mt-3">
           <div class="flex flex-col gap-2">
             <Label for="email">Email</Label>
-            <InputText id="email" type="email" />
+            <InputText id="email" type="email" v-model="email" />
           </div>
           <div class="flex flex-col gap-2">
             <div class="flex items-center justify-between">
               <Label for="password">Password</Label>
               <Button variant="link">Forgot password?</Button>
             </div>
-            <InputText id="password" type="password" />
+            <InputText id="password" type="password" v-model="password" />
           </div>
         </form>
       </template>
       <template #footer>
         <div class="flex flex-col gap-4 mt-10">
-          <Button class="w-full">Login</Button>
+          <Button class="w-full" onClick="login">Login</Button>
+          <Button severity="secondary" variant="outlined" class="w-full">Login with Google</Button>
+          <div class="mt-2 text-center text-surface-500 text-sm">
+            Don't have an account?
+            <Button variant="link" class="p-0">Sign up</Button>
+          </div>
         </div>
       </template>
     </Card>
   </div>
 </template>
+
+<script setup>
+const email = ref('');
+const password = ref('');
+
+const login = async () => {
+  const { token } = await $fetch('/api/login', {
+    method: 'POST',
+    body: {email, password}
+  });
+};
+</script>
