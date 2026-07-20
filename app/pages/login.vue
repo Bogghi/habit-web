df73e9a4572a7a7b9253d948f8fa0b909d7094af
Bogghi <m.borghi@tecnocasa.com>
2026-07-20 15:57:41 +0200

    added first version of the login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -4,7 +4,24 @@
       <template #title>Login</template>
       <template #subtitle>Sig in with your credentials</template>
       <template #content>
-
+        <form class="space-y-6 mt-3">
+          <div class="flex flex-col gap-2">
+            <Label for="email">Email</Label>
+            <InputText id="email" type="email" />
+          </div>
+          <div class="flex flex-col gap-2">
+            <div class="flex items-center justify-between">
+              <Label for="password">Password</Label>
+              <Button variant="link">Forgot password?</Button>
+            </div>
+            <InputText id="password" type="password" />
+          </div>
+        </form>
+      </template>
+      <template #footer>
+        <div class="flex flex-col gap-4 mt-10">
+          <Button class="w-full">Login</Button>
+        </div>
       </template>
     </Card>
   </div>
